dc8cf39d767f68e27b3fc1ac69f7aa3dde8bae70
noura nabil <nouranabile06@gmail.com>
2026-09-20 14:45:50 +0300

    add module  style on header insted of inline style use it as object

--- src/components/tt/Header.jsx
+++ src/components/tt/Header.jsx
@@ -1,8 +1,9 @@
 import Navbar from "./Navbar"
-
+import style from './header.module.css'
 const Header = ()=>{
     return(
         <header style={{display : 'flex', justifyContent : 'space-around'}}>
+            {/* <header className={style.header}> */}
             <a href="/">Logo</a>
             <Navbar/>
         </header>
